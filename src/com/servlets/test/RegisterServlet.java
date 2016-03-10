d69a63b8d64a2dcbc7b848373b6a8f7950302224
drilldash <azimov1990@yandex.ru>
2016-03-10 21:37:34 +0300

    First Steps on Servlets

---
+++ src/com/servlets/test/RegisterServlet.java
@@ -0,0 +1,46 @@
+package com.servlets.test;
+
+
+import java.io.*;
+import java.net.Socket;
+import java.net.SocketException;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+
+public class RegisterServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+
+        String user = request.getParameter("name");
+        String pass = request.getParameter("pass");
+        try {
+
+            Socket serverSocket = new Socket("localhost", 15151);
+            BufferedReader fromSocketServer = new BufferedReader(new InputStreamReader(serverSocket.getInputStream()));
+            PrintWriter toSocketServer = new PrintWriter(serverSocket.getOutputStream(), true);
+
+
+            toSocketServer.println("register_user " + user + " " + pass);
+            String socketServerResponse = fromSocketServer.readLine();
+
+            System.out.println(socketServerResponse);
+
+            out.println("<html><body>");
+            out.println(socketServerResponse);
+            out.println("</body></html>");
+
+
+        } catch (SocketException socketException) {
+            socketException.printStackTrace();
+        } catch (Exception se) {
+            se.printStackTrace();
+
+        }
+    }
+}
+
+
---
+++ web/Hello.jsp
@@ -0,0 +1,22 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: verlorener
+  Date: 09.03.16
+  Time: 19:44
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<%
+    int a = 3;
+    int b = 4;
+    int c = a + b;
+    out.print( "c="+c);
+%>
+
+</body>
+</html>
